fa17691b8c0aff8cc59da9c09faa791aad30a3e2
Waterticket <matthew218@naver.com>
2023-04-30 13:13:41 +0000

    Feat: download_url이 상대경로로 생성되는 이슈 해결

--- controllers/file.php
+++ controllers/file.php
@@ -20,7 +20,7 @@ class File extends Base
 		$oFileController->procFileUpload();
 
 		Context::setResponseMethod('JSON');
-		$this->add('link', $oFileController->get('download_url'));
+		$this->add('link', '/'.$oFileController->get('download_url'));
 		$this->add('file-srl', $oFileController->get('file_srl'));
 		$this->add('upload-target-srl', $oFileController->get('upload_target_srl'));
 	}
